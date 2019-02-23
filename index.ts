cc1d8c9e0638eb267b7650dd213e9ecd26e1a626
Emerick Bosch <xpc.dev@gmail.com>
2019-02-23 23:29:47 +0200

    Add "bank" as a script option
    This currently supports "fnb" and "standardbank".

--- index.ts
+++ index.ts
@@ -2,11 +2,21 @@ import * as cli from "commander";
 
 cli
   .version("1.0.0")
-  .option("-p, --person <name>", "The person to greet")
+  .option(
+    "-b, --bank <bank>",
+    "The bank who's statement will be parsed",
+    /^(fnb|standardbank)$/i,
+    false
+  )
   .parse(process.argv);
+
+if (!cli.bank) {
+  console.error("Invalid bank name. Type --help for more details.");
+  process.exit(1);
+}
 
 function greeter(person) {
   return "Hello " + person + "!";
 }
 
-console.log(greeter(cli.person));
+console.log(greeter(cli.bank));
