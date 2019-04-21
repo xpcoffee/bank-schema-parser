ccdb21c361323d9e9d38cc2eeee36bbe4874554f
Emerick Bosch <xpc.dev@gmail.com>
2019-04-21 17:33:03 +0200

    Clarify getStatementParser
    It took me a while to find out what this was doing again. It was
    deserving of some more documentation.

--- src/statement.ts
+++ src/statement.ts
@@ -9,24 +9,28 @@ export function getEmptyStatement(): Statement {
     account: UKNOWN,
     bank: UKNOWN,
     transactions: [],
-    parsingErrors: []
+    parsingErrors: [],
   };
 }
 
-export function getStatementParser<T>(
-  parsingFunction: ParsingFunction
-): StatementParser {
-  return async (file: string) => {
-    const fileStream = fs.createReadStream(file);
-    const rl = readline.createInterface({
-      input: fileStream,
-      crlfDelay: Infinity
-    });
+/**
+ * Given a line-parsing function, this returns a function that will parse a the file at a given path.
+ *
+ * @param parsingFunction The function to use to parse each line of the file.
+ * @returns a function that, given a path to a file, will parse the file into a Statement object.
+ */
+export const getStatementParser = <T>(parsingFunction: ParsingFunction): StatementParser => async (
+  filePath: string,
+) => {
+  const fileStream = fs.createReadStream(filePath);
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
 
-    let memo = getEmptyStatement();
-    for await (const line of rl) {
-      memo = parsingFunction(line, memo);
-    }
-    return memo;
-  };
-}
+  let memo = getEmptyStatement();
+  for await (const line of rl) {
+    memo = parsingFunction(line, memo);
+  }
+  return memo;
+};
