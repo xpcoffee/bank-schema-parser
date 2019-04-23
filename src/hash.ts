be1142e824b21036e9093d21116519a1af380fcc
Emerick Bosch <xpc.dev@gmail.com>
2019-04-23 22:33:32 +0200

    Fix the hash function

--- src/hash.ts
+++ src/hash.ts
@@ -1,10 +1,12 @@
 export default (str: string): number => {
-  var hash = 0,
-    i,
-    chr;
+  let hash = 0;
+  let chr;
 
-  if (str.length === 0) return hash;
-  for (i = 0; i < this.length; i++) {
+  if (str.length === 0) {
+    return hash;
+  }
+
+  for (let i = 0; i < str.length; i++) {
     chr = str.charCodeAt(i);
     hash = (hash << 5) - hash + chr;
     hash |= 0; // Convert to 32bit integer
