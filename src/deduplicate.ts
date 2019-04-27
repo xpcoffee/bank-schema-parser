5d040d584286e80346238c26039a9e76235f100e
Emerick Bosch <xpc.dev@gmail.com>
2019-04-27 19:20:13 +0200

    Improve docs around deduplication
    Otherwise I'm going to be 100% confused when I look at this in 6months.

--- src/deduplicate.ts
+++ src/deduplicate.ts
@@ -3,12 +3,19 @@ import { Statement, Transaction } from "./types";
 type Dict<T> = { [key: string]: T };
 
 /**
- * This deduplicates hashes in the set.
+ * This deduplicates hashes of transactions in a Statement.
  *
- * Context: It's possible to have multiple transactions on the same day with the same data.
- * This happens most often with bank charges.
+ * Context: It's possible to have multiple transactions on the same day with the same data;
+ * this happens most often with bank charges. So it is necessary to be able make those transaction
+ * hashes distinct.
+ *
+ * Properties we want:
+ *
+ *  - idempotent for transactions: running this over two statements that have the same duplicate transactions
+ *    should result in the same set of deduplicated transactions. This is important for safely working with
+ *    different types of statement (monthly statements, yearly statements, etc).
  */
-const deduplicate = (untreatedStatement: Statement): Statement => {
+export default (untreatedStatement: Statement): Statement => {
   const statement = Object.assign({}, untreatedStatement);
   const dict: Dict<Transaction> = {};
 
@@ -31,5 +38,3 @@ const deduplicate = (untreatedStatement: Statement): Statement => {
   statement.transactions = Object.values(dict);
   return statement;
 };
-
-export default deduplicate;
