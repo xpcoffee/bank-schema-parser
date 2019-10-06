f0fe656c5253df086b1c11705b05f8163690b69f
Emerick Bosch <xpc.dev@gmail.com>
2019-10-06 16:39:31 +0200

    Improve the readability of the statement parser

--- src/statement.ts
+++ src/statement.ts
@@ -2,12 +2,12 @@ import * as fs from "fs";
 import * as readline from "readline";
 import { Statement, ParsingFunction, StatementParser } from "./types";
 
-export const UKNOWN = "UNKNOWN";
+export const UNKNOWN = "UNKNOWN";
 
 export function getEmptyStatement(): Statement {
   return {
-    account: UKNOWN,
-    bank: UKNOWN,
+    account: UNKNOWN,
+    bank: UNKNOWN,
     transactions: [],
     parsingErrors: [],
   };
@@ -19,18 +19,17 @@ export function getEmptyStatement(): Statement {
  * @param parsingFunction The function to use to parse each line of the file.
  * @returns a function that, given a path to a file, will parse the file into a Statement object.
  */
-export const getStatementParser = <T>(parsingFunction: ParsingFunction): StatementParser => async (
-  filePath: string,
-) => {
-  const fileStream = fs.createReadStream(filePath);
-  const rl = readline.createInterface({
-    input: fileStream,
-    crlfDelay: Infinity,
-  });
+export function getStatementParser<T>(parsingFunction: ParsingFunction): StatementParser {
+  return async function parseStatement(filePath: string) {
+    const rl = readline.createInterface({
+      input: fs.createReadStream(filePath),
+      crlfDelay: Infinity,
+    });
 
-  let memo = getEmptyStatement();
-  for await (const line of rl) {
-    memo = parsingFunction(line, memo);
-  }
-  return memo;
-};
+    let memo = getEmptyStatement();
+    for await (const line of rl) {
+      memo = parsingFunction(line, memo);
+    }
+    return memo;
+  };
+}
