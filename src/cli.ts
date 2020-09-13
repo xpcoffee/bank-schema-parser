7203d367b8dd04385acb2e6af464ef548e5cd4db
Emerick Bosch <xpc.dev@gmail.com>
2020-09-13 20:55:25 +0200

    Trigger CLI from a binary
    Still hitting issues installing the CLI with the entrypiont being a
    typescript file - the artifact doesn't exist until after postinstall so the
    install step that sets up permissions fails when trying to make the script
    executable.
    
    This change attempts to work around this by having the script entrypoint be a binary which can be copied over before the chmod step runs.

--- src/cli.ts
+++ src/cli.ts
@@ -3,45 +3,47 @@
 
 import * as program from "commander";
 
-import { ParseFileParams, parseFromFile, ParseParams } from "./parser";
+import { ParseFileParams, parseFromFile } from "./parser";
 
-// Parse command-line input
-program
-  .version("1.0.0")
-  .name("bank-schema-parser")
-  .usage("--bank <bank> --filePath <filePath> [--type <type>]")
-  .option("-b, --bank <bank>", "The bank who's statement will be parsed", /^(fnb|standardbank)$/i, false)
-  .option("-f, --filePath <filePath>", "The path to the file that should be parsed")
-  .option(
-    "-t, --type <type>",
-    "Use to specify the type of input file. Can be DEFAULT, TRANSACTION_HISTORY or HANDMADE. Uses DEFAULT if the option is unspecified.",
-  )
-  .parse(process.argv);
+export function run(proc: NodeJS.Process) {
+  // Parse command-line input
+  program
+    .version("1.0.0")
+    .name("bank-schema-parser")
+    .usage("--bank <bank> --filePath <filePath> [--type <type>]")
+    .option("-b, --bank <bank>", "The bank who's statement will be parsed", /^(fnb|standardbank)$/i, false)
+    .option("-f, --filePath <filePath>", "The path to the file that should be parsed")
+    .option(
+      "-t, --type <type>",
+      "Use to specify the type of input file. Can be DEFAULT, TRANSACTION_HISTORY or HANDMADE. Uses DEFAULT if the option is unspecified.",
+    )
+    .parse(proc.argv);
 
-if (!program.bank && !program.filePath) {
-  program.help();
+  if (!program.bank && !program.filePath) {
+    program.help();
+  }
+
+  if (!program.bank) {
+    console.error("Invalid bank name. Type --help for more details.");
+    proc.exit(1);
+  }
+
+  if (!program.filePath) {
+    console.error("Invalid bank statement file. Type --help for more details.");
+    proc.exit(1);
+  }
+
+  const params = (program as any) as CliParams;
+
+  try {
+    const printJson = (s: {}) => console.log("%j", s);
+
+    parseFromFile({ bank: params.bank, type: params.type, filePath: params.filePath })
+      .then(printJson)
+      .catch(console.error);
+  } catch (e) {
+    console.error(`[ERROR] ${e}`);
+  }
+
+  interface CliParams extends ParseFileParams {}
 }
-
-if (!program.bank) {
-  console.error("Invalid bank name. Type --help for more details.");
-  process.exit(1);
-}
-
-if (!program.filePath) {
-  console.error("Invalid bank statement file. Type --help for more details.");
-  process.exit(1);
-}
-
-const params = (program as any) as CliParams;
-
-try {
-  const printJson = (s: {}) => console.log("%j", s);
-
-  parseFromFile({ bank: params.bank, type: params.type, filePath: params.filePath })
-    .then(printJson)
-    .catch(console.error);
-} catch (e) {
-  console.error(`[ERROR] ${e}`);
-}
-
-interface CliParams extends ParseFileParams {}
